80f5775983e505c12d98c2dca07671dbbdf06638
Prashant <devopswithprashant@gmail.com>
2025-05-01 23:54:05 +0530

    Revert "add test"
    This reverts commit 32eb9d2031c2c2a98a43a112c104c3deada5ab2e.

--- src/setupTests.js
+++ src/setupTests.js
@@ -2,18 +2,4 @@
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
-
 import '@testing-library/jest-dom';
-import { TextEncoder, TextDecoder } from 'util';
-import { ReadableStream } from 'web-streams-polyfill';
-import { server } from './mocks/server';
-
-// Polyfill TextEncoder/TextDecoder
-global.TextEncoder = TextEncoder;
-global.TextDecoder = TextDecoder;
-global.ReadableStream = ReadableStream;
-
-// MSW setup
-beforeAll(() => server.listen());
-afterEach(() => server.resetHandlers());
-afterAll(() => server.close());
